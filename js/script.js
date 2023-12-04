e2407de9a73a077538b761c41f6965b3ceaf212b
Gobinda Das <gobinda993720@gmail.com>
2023-12-04 18:35:16 +0530

    add video and a follower

---
+++ js/script.js
@@ -0,0 +1,23 @@
+let videoCon = document.querySelector("#video-container");
+let mouseFollower = document.querySelector("#play");
+
+videoCon.addEventListener("mouseenter", ()=> {
+    gsap.to(mouseFollower, {
+        opacity: 1,
+        scale: 1,
+    })
+})
+
+videoCon.addEventListener("mouseleave", ()=> {
+    gsap.to(mouseFollower, {
+        opacity: 0,
+        scale: 0,
+    })
+})
+
+videoCon.addEventListener("mousemove", (dets)=> {
+    gsap.to(mouseFollower, {
+        left: dets.x,
+        top: dets.y,
+    })
+})
