af7b8363c0c52692fdb0932cde4de90f3b72d688
Gobinda Das <gobinda993720@gmail.com>
2023-12-04 18:38:34 +0530

    pul video follower animation inside a function

--- js/script.js
+++ js/script.js
@@ -1,23 +1,26 @@
-let videoCon = document.querySelector("#video-container");
-let mouseFollower = document.querySelector("#play");
+function videoConAnimation() {
+    let videoCon = document.querySelector("#video-container");
+    let mouseFollower = document.querySelector("#play");
 
-videoCon.addEventListener("mouseenter", ()=> {
-    gsap.to(mouseFollower, {
-        opacity: 1,
-        scale: 1,
+    videoCon.addEventListener("mouseenter", () => {
+        gsap.to(mouseFollower, {
+            opacity: 1,
+            scale: 1,
+        })
     })
-})
 
-videoCon.addEventListener("mouseleave", ()=> {
-    gsap.to(mouseFollower, {
-        opacity: 0,
-        scale: 0,
+    videoCon.addEventListener("mouseleave", () => {
+        gsap.to(mouseFollower, {
+            opacity: 0,
+            scale: 0,
+        })
     })
-})
 
-videoCon.addEventListener("mousemove", (dets)=> {
-    gsap.to(mouseFollower, {
-        left: dets.x,
-        top: dets.y,
+    videoCon.addEventListener("mousemove", (dets) => {
+        gsap.to(mouseFollower, {
+            left: dets.x,
+            top: dets.y,
+        })
     })
-})
+}
+videoConAnimation();
